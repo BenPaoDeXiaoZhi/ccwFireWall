fb789d1963b8d04e327fb5ccae7febe43f6d4ecf
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-02-26 18:41:49 +0800

    更新 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import monkey from "vite-plugin-monkey";
 
 // https://vite.dev/config/
-export default defineConfig(({mode}=>{return {
+export default defineConfig(({mode})=>{return {
   build: {
     minify: mode!="dev",
   },
