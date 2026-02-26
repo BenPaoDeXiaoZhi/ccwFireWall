b582498fb7d82531b4563f956a266b277352c35c
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-02-26 23:48:44 +0800

    更新 plugin.ts

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,19 @@
 import type { Component } from 'svelte';
+
+export type FunctionalComponent = ({
+  vm: GandiVM,
+  root: HTMLElement,
+}) => Function;
+
 export class Plugin {
   name: string;
   icon: string|Component;
   main: Component;
-  constructor(name:string, icon: string|Component, main: Component){
+  constructor(
+    name:string,
+    icon: string|Component,
+    main: Component|FunctionalComponent
+  ){
     this.name = name;
     this.icon = icon;
     this.main = main;
