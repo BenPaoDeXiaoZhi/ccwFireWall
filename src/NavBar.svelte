331fdef637d9b73d1711a6d51bbf7b3f6eda026f
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-02-26 20:15:39 +0800

    更新 NavBar.svelte

--- src/NavBar.svelte
+++ src/NavBar.svelte
@@ -1,1 +1,15 @@
-//
+<script lang="ts">
+  import type { Plugin } from "./plugin";
+  type Props = {
+    plugins: Plugin[]
+  }
+  const { plugins=[] } = $props();
+</script>
+
+<nav>
+  {#each plugins as plugin}
+    <ul>
+      <span>{plugin.name}</span>
+    </ul>
+  {/each}
+</nav>
