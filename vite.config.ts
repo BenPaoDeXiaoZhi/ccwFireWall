7a1e081a5932e18f08b6d1d536f18a64138bfc36
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-02-26 20:39:34 +0800

    更新 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import monkey from "vite-plugin-monkey";
 
+const VERSION="0.0.0"
+
 // https://vite.dev/config/
 export default defineConfig(({mode})=>{return {
   build: {
-    minify: mode!="dev",
+    minify: mode!="development",
   },
   plugins: [
     svelte(),
@@ -13,6 +15,7 @@ export default defineConfig(({mode})=>{return {
       entry: "./src/main.ts",
       userscript: {
         name: "CCW Firewall",
+        version: `${VERSION}-${new Date().toGMTString()}`,
         author: "Meng Fuzi",
         match: "https://*.ccw.site/*",
         grant: [
