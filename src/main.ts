07845e6399aaac6454d5eb3976eff86da53c84c7
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-02-26 23:36:46 +0800

    更新 main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { mount } from "svelte";
 import { startTrap } from "./trapVM";
 import App from "./App.svelte";
+import plugins from "../plugins/plugin-manifest";
 
 const app = mount(App, {
   target: document.body,
+  props: {
+    plugins,
+  },
 });
 
 const useBind = localStorage.getItem("firewall.useBind") == "true" ? true : false;
