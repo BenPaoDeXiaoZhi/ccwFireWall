4e81746e1e9773cb06b104dd643efdebf0d16b50
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-02-27 10:08:50 +0800

    创建 devTools/Main.svelte

---
+++ plugins/devTools/Main.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  const { vm } = $props();
+  let vmKey=$state("vm");
+</script>
+
+<label>
+  vm保存位置:
+  <input type="text" bind:value={vmKey}>
+</label>
+
+<button
+  onclick={()=>{
+    window[vmKey]=vm;
+  }}
+  disabled={!vm}
+>
+  保存vm(vm{!vm ? "未" : "已"}获取到)
+</button>
