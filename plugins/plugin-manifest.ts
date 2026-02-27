396eb5024cda33f86710d1d098aa12aca756cab8
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-02-27 11:49:36 +0800

    更新 plugin-manifest.ts

--- plugins/plugin-manifest.ts
+++ plugins/plugin-manifest.ts
@@ -3,6 +3,5 @@ import { Plugin } from "../src/plugin";
 import DevTools from "./devTools/Main.svelte";
 
 export default [
-  new Plugin("test","",()=>(()=>null)),
   new Plugin("dev tools","",DevTools),
 ]
