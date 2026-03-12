7427446ce077e35368de046790f4b633696ccbd1
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-03-12 21:54:30 +0800

    更新 store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,12 @@
 import { writable } from "svelte/store";
 const configKey = "firewall.config"
-let origConfig = {};
+let origConfig: Record<string, any> = {};
 try {
     origConfig = JSON.parse(localStorage.getItem(configKey);
 }catch(e) {
     console.warn("firewall:", e);
 };
-export const config = writable<Record<string, any>>({});
+export const config = writable(origConfig);
 config.subscribe((cfg)=>{
     localStorage.setItem(configKey, JSON.stringify(cfg));
 });
