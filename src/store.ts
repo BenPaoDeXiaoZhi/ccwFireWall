fa121a6c77f4c6fab52136319a607abc8cf2db5c
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-03-12 21:53:46 +0800

    更新 store.ts

--- src/store.ts
+++ src/store.ts
@@ -1,2 +1,12 @@
 import { writable } from "svelte/store";
-export const vm = writable<GandiVM>();
+const configKey = "firewall.config"
+let origConfig = {};
+try {
+    origConfig = JSON.parse(localStorage.getItem(configKey);
+}catch(e) {
+    console.warn("firewall:", e);
+};
+export const config = writable<Record<string, any>>({});
+config.subscribe((cfg)=>{
+    localStorage.setItem(configKey, JSON.stringify(cfg));
+});
