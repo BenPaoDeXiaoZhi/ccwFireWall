74bd5b05fdea424ad8b831c9c92814c57e7c8374
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-04-13 21:14:58 +0800

    更新 adapter.ts

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,3 +1,18 @@
+import { config } from "#src/store";
+import { get } from "svelte/store";
+
 if (!("unsafeWindow" in window)) {
   (window as any).unsafeWindow = window;
-}
+};
+
+const origin = {
+  console
+};
+
+config.subscribe((config)=>{
+  if(!!config.consoleRedirect){
+    console = unsafeWindow.console;
+  }else{
+    console = origin.console;
+  }
+});
