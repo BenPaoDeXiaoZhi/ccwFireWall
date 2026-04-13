0d5b68d08454cf1341828180c5a12f03b59c8ba8
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-04-13 19:36:02 +0800

    更新 main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { mount } from "svelte";
 import App from "./App.svelte";
 import plugins from "../plugins/plugin-manifest";
-import "./adapter";
+import "./preload";
 
 let { document, localStorage } = unsafeWindow;
 
