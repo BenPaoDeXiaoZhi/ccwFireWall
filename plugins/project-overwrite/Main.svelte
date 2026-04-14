30cfb32ace860ce50c59520fad72a87a352e2cc8
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-04-14 22:07:10 +0800

    更新 Main.svelte

--- plugins/project-overwrite/Main.svelte
+++ plugins/project-overwrite/Main.svelte
@@ -2,16 +2,25 @@
   import type { PluginContext } from "#src/plugin";
   import { config } from "#src/store";
   const { vm }: PluginContext = $props();
-  export const isInEditor = document.location.pathname.startsWith("/gandi/project/")
+  export const isInEditor = document.location.pathname.startsWith("/gandi/project/");
+  export let noAutoSave = $state(true);
 </script>
 
 {#if isInEditor}
-  <li id="vm">
+  <li id="overwrite">
     <label>
       替换sb3文件
       <input type="checkbox" bind:checked={$config["overwrite.enable"]} />
     </label>
   </li>
+  {#if $config["overwrite.enable"]}
+    <li id="autosave">
+      <label>
+        禁用自动保存
+        <input type="checkbox" bind:checked={noAutoSave} />
+      </label>
+    </li>
+  {/if}
   <li id="reload">
     <button onclick={() => document.location.reload()}>重新加载作品</button>
   </li>
