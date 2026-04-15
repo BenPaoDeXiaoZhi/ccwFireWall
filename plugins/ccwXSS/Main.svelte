748df7114ac9a5549a606baf08fea47dca780cf3
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-04-15 20:36:29 +0800

    更新 Main.svelte

--- plugins/ccwXSS/Main.svelte
+++ plugins/ccwXSS/Main.svelte
@@ -1,1 +1,22 @@
-help
+<li id="input">
+  <label>
+    输入js代码
+    <input />
+  </label>
+</li>
+<li id="output">
+  <label>
+    编译后结果
+    <input />
+  </label>
+</li>
+
+<style>
+  li {
+    list-style: none;
+    font-size: 12px;
+  }
+  button {
+    font-size: 12px;
+  }
+</style>
