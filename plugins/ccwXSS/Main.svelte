52065023dc36e66445e2926949c2bddb3b29739a
MengFuziComing~ <121215648+BenPaoDeXiaoZhi@users.noreply.github.com>
2026-04-15 21:01:25 +0800

    更新 Main.svelte

--- plugins/ccwXSS/Main.svelte
+++ plugins/ccwXSS/Main.svelte
@@ -1,13 +1,29 @@
+<script lang="ts">
+  import { PluginContext } from "#src/plugin";
+
+  function escapeAscii(char: string){
+    return `\x${char.charCodeAt(0).toString(16).padStart(2, "0")}`;
+  }
+  const needEscape = Array.from(`()[]{}'"`);
+  
+  let input = $state("");
+  let output = $derived.by(()=>{
+    const encrypted = needEscape.reduce((code, char)=>{
+      return code.replaceAll(char, escapeAscii(char));
+    }
+  }, input);
+</script>
+
 <li id="input">
   <label>
     输入js代码
-    <input />
+    <input bind:value={input}/>
   </label>
 </li>
 <li id="output">
   <label>
     编译后结果
-    <input />
+    <input bind:value={output}/>
   </label>
 </li>
 
